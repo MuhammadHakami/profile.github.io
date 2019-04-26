efeec296cf7b522d4119831f2396ca8ecd3b4ce2
Muhammad-Hakami <engmuhammadhakami@gmail.com>
2019-04-26 11:17:24 +0300

    Updates

---
+++ precache-manifest.9ed48e2f7eed1a4304422f487a0d71af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fccbad4aaa6a48feb0ff7c60522bbfa1",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "fdad753cdae4e91ca8f5",
+    "url": "/profile/static/css/main.24f91433.chunk.css"
+  },
+  {
+    "revision": "bfff185d56db53e57e84",
+    "url": "/profile/static/js/2.8e8d0f61.chunk.js"
+  },
+  {
+    "revision": "fdad753cdae4e91ca8f5",
+    "url": "/profile/static/js/main.852f21f9.chunk.js"
+  },
+  {
+    "revision": "189fd871db148202566e",
+    "url": "/profile/static/js/runtime~main.cc8686f1.js"
+  },
+  {
+    "revision": "2f4b192225da5ce9652a2753b22dd5e7",
+    "url": "/profile/static/media/background.2f4b1922.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/profile/precache-manifest.c86fda46963cafd10c872f8dd9123fe3.js"
+  "/profile/precache-manifest.9ed48e2f7eed1a4304422f487a0d71af.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/profile/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/profile/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
